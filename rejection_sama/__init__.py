6b5a04e26e3a8d1c90a09f1c693c0e2ac798d067
Devany West <dwest@rockefeller.edu>
2024-07-17 12:05:28 -0400

    Fixed goal change and messages, added help messages to all commands

--- rejection_sama/__init__.py
+++ rejection_sama/__init__.py
@@ -11,23 +11,29 @@ col_names = ['Guild','Goal','Rejections','Celebrations','Reset']
 default_goal = 25
 
 # record a rejection, and celebrate if you've hit your goal
-@bot.command(name="damage")
+@bot.command(name="damage", brief="To log a rejection, use the command *!damage 1* for one rejection, or change the number for multiple.", description="To log a rejection, use the command *!damage 1* for one rejection, or change the number for multiple.")
 async def damage(context, num: int):
     celebrate = False
     rejections_df, guild_id, goal, rejections, celebrations, reset = get_guild_info(context) 
     for r in range(num):
         rejections += 1
-        if (rejections-reset) % goal == 0:
+        if int(rejections-reset) % goal == 0:
             celebrate = True
     rejections_df.loc[rejections_df['Guild'] == guild_id, 'Rejections'] = rejections
     rejections_df.to_csv(rejections_file, index=False)
     if celebrate == True:
-        await context.reply(f"You are at {rejections} rejections, and that means: congratulations, you've reached another {goal} rejections!  Rejection-sama demands that you go celebrate!")
+        reset = rejections
+        celebrations += 1
+        rejections_df.loc[rejections_df['Guild'] == guild_id, 'Reset'] = reset
+        rejections_df.loc[rejections_df['Guild'] == guild_id, 'Celebrations'] = celebrations
+        await context.reply(f"You are at {rejections} total rejections, and that means: congratulations, you've reached another {goal} rejections!  Rejection-sama demands that you go celebrate!")
     else:
-        await context.reply(f"Rejection-sama thanks you for the rejections!  You're at {rejections} rejections.")
+        await context.reply(f"Rejection-sama thanks you for the rejections!  You're at {rejections} total rejections.") 
+    rejections_df.loc[rejections_df['Guild'] == guild_id, 'Rejections'] = rejections
+    rejections_df.to_csv(rejections_file, index=False)
 
 # remove rejections if you accidentally on purpose add too many 
-@bot.command(name="oops")
+@bot.command(name="oops", brief="To remove a rejection, use the command *!oops 1* to remove one rejection, or change the number for multiple.", description="To remove a rejection, use the command *!oops 1* to remove one rejection, or change the number for multiple.")
 async def oops(context, num: int):
     rejections_df, guild_id, goal, rejections, celebrations, reset = get_guild_info(context)
     rejections -= num
@@ -35,29 +41,30 @@ async def oops(context, num: int):
         rejections = 0
         await context.reply(f"Rejection-sama cannot give you negative rejections, much as Rejection-sama would like... you are now at 0 rejections.")
     else:
-        await context.reply(f"Rejection-sama has removed {num} rejections!  You are now at {rejections} rejections.")
+        await context.reply(f"Rejection-sama has removed {num} rejections!  You are now at {rejections}total rejections.")
     rejections_df.loc[rejections_df['Guild'] == guild_id, 'Rejections'] = rejections
     rejections_df.to_csv(rejections_file, index=False)
 
 # print the current number of rejections and the number of rejections left to the goal
-@bot.command(name="progress")
+@bot.command(name="progress",brief="To check the status of your rejections and how close you are to your goal, use the command *!progress*.",description="To check the status of your rejections and how close you are to your goal, use the command *!progress*.")
 async def progress(context):
-    rejections_df, guild_id, goal, rejections = get_guild_info(context)
-    to_go = goal - (rejections-reset) % goal
-    await context.reply(f"You are at {rejections} rejections.  {to_go} rejections left to your next goal!")
+    rejections_df, guild_id, goal, rejections, celebrations, reset = get_guild_info(context)
+    to_go = goal - int(rejections-reset) % goal
+    await context.reply(f"You are at {rejections} total rejections and have celebrated {celebrations} times.  Your last celebration was at {reset} rejections.  You have {to_go} rejections left to your next goal of {goal} rejections!")
 
 # set goal number of rejections 
 # if the new goal means a celebration has been reached, a celebration will be prompted, but only one
-# TODO: Make sure this behaves the way you think it should!
-@bot.command(name="goal")
+@bot.command(name="goal", brief="To change your goal, use the command *!goal 50* to set your goal to 50 rejections, or use another number.", description="To change your goal, use the command *!goal 50* to set your goal to 50 rejections, or use another number.  The goal is always counted as the number of rejections from your last celebration.  If you change your goal while progressing towards your next celebration and the new goal is lower than the current number of rejections since that last celebration, you'll immediately earn a celebration and then start counting again from there.  For instance, if you are trying to get to 10, and then set your goal to 5 when you are at 7, you earn a celebration at 7 and will earn your next celebration at 12.")
 async def goal(context, num: int):
     rejections_df, guild_id, goal, rejections, celebrations, reset = get_guild_info(context)
     if int((rejections-reset)/goal) < int((rejections-reset)/num):
         await context.reply(f"By resetting your goal to {num}, you have reached another celebration!  You may go celebrate if you wish!")
-        await context.reply(f"Your goal is now to reach another {num} rejections, you have {goal} rejections left to your next goal!")
-        reset = rejections
-    to_go = num - (rejections-reset) % num
-    await context.reply(f"Your goal is now to reach another {num} rejections, you have {to_go} rejections left to your next goal!")
+        await context.reply(f"Your goal is now to reach another {num} rejections from here.")
+        rejections_df.loc[rejections_df['Guild'] == guild_id, 'Celebrations'] = celebrations + 1
+    else:
+        to_go = num - int(rejections-reset) % num
+        await context.reply(f"Your goal is now to reach another {num} rejections, you have {to_go} rejections left to your next goal!")
+    # TODO: reset needs to equal last celebration!!!
     rejections_df.loc[rejections_df['Guild'] == guild_id, 'Goal'] = num
     rejections_df.to_csv(rejections_file, index=False)    
 
@@ -67,19 +74,22 @@ async def on_ready():
         
 def get_guild_info(context):
     guild_id = context.message.guild.id
-    rejections_df = pd.read_csv(rejections_file)
+    try:
+        rejections_df = pd.read_csv(rejections_file)
+    except:
+        to_df = {}
+        for i in col_names:
+            to_df[i] = []
+        rejections_df = pd.DataFrame(to_df)
     if guild_id not in rejections_df['Guild'].values:
-        guild_to_append = pd.DataFrame({col_names[0]:[guild_id],col_names[1]:[default_goal],col_names[2]:[0]})
-        if rejections_df.empty:
-            rejections_df = guild_to_append
-        else:
-            rejections_df = pd.concat([rejections_df,guild_to_append],names=col_names) 
+        guild_to_append = pd.DataFrame({col_names[0]:[guild_id],col_names[1]:[default_goal],col_names[2]:[0],col_names[3]:[0],col_names[4]:[0]})
+        rejections_df = pd.concat([rejections_df,guild_to_append],names=col_names) 
         rejections_df.to_csv(rejections_file, index=False)
     guild_loc = rejections_df['Guild'] == guild_id 
     rejections = rejections_df.loc[guild_loc, 'Rejections'].item()
     goal = rejections_df.loc[guild_loc, 'Goal'].item()
     celebrations = rejections_df.loc[guild_loc, 'Celebrations'].item()
-    resets = rejections_df.loc[guild_loc, 'Resets'].item() 
+    resets = rejections_df.loc[guild_loc, 'Reset'].item() 
     return(rejections_df, guild_id, goal, rejections, celebrations, resets)
 
 def main():
