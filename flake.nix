4a0c88c43755f3bd54e7d300ad92f5fe04e454cf
Aaron L. Zeng <me@bcc32.com>
2024-07-18 13:22:30 -0400

    Add pandas dependency

--- flake.nix
+++ flake.nix
@@ -1,35 +1,46 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in {
+    in
+    {
       packages = forAllSystems (system: {
-        default = with pkgs.${system};
+        default =
+          with pkgs.${system};
           with python39Packages;
           buildPythonApplication {
             pname = "rejection-sama";
             version = "0.1.0";
             src = ./.;
             format = "pyproject";
-            propagatedBuildInputs = [ discordpy flit ];
+            propagatedBuildInputs = [
+              discordpy
+              flit
+              pandas
+            ];
           };
       });
 
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system};
-            [
-              (python39.withPackages (ps:
-                with ps; [
-                  black
-                  self.packages.${system}.default.buildInputs
-                ]))
-            ];
+          packages = with pkgs.${system}; [
+            (python39.withPackages (
+              ps: with ps; [
+                black
+                self.packages.${system}.default.buildInputs
+              ]
+            ))
+          ];
         };
       });
     };
